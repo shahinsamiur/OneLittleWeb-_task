614cfd20b101b19c03a3381fecf88415b4f13965
shahinsamiur <shahinsamiur@yahoo.com>
2026-04-03 03:55:01 +0600

    fix: container wraper fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Service from "../components/service";
 export default function page() {
   return (
     <ContainerWraper>
-      <div className="min-h-screen">
+      <div className="min-h-screen flex flex-col justify-center items-center w-full">
         <Service />
       </div>
     </ContainerWraper>
--- src/utils/containerWraper.tsx
+++ src/utils/containerWraper.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function ContainerWraper({ children }: { children: React.ReactNode }) {
   return (
-    <div className="container mx">
+    <div className="container mx-auto">
       <section>{children}</section>
     </div>
   );
