c56ba041a908b61d13aaf65c1359d815c32166d0
shahinsamiur <shahinsamiur@yahoo.com>
2026-04-03 12:47:43 +0600

    feat: tools  section design complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 import { ContainerWraper } from "@/src/utils";
 import Service from "../components/service";
 import PriceSection from "../components/pakageSection";
+import Tools from "../components/tools";
 export default function page() {
   return (
     <ContainerWraper>
       <div className="min-h-screen flex flex-col justify-center items-center w-full gap-20">
         <Service />
         <PriceSection />
+        <Tools />
       </div>
     </ContainerWraper>
   );
---
+++ src/components/tools.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Image from "next/image";
+export default function Tools() {
+  const tools = [
+    "/tools/tool1.png",
+    "/tools/tool2.png",
+    "/tools/tool3.png",
+    "/tools/tool4.png",
+    "/tools/tool5.png",
+    "/tools/tool6.png",
+    "/tools/tool7.png",
+    "/tools/tool8.png",
+    "/tools/tool8.png",
+  ];
+  return (
+    <section className="flex flex-col justify-center items-center gap-6 w-full py-10">
+      <h1 className="w-full text-center text-3xl font-semibold">
+        Our <span className="text-foreground">Tools</span>
+      </h1>
+
+      <p className="text-center font-medium w-1/2">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
+      <div className="bg-[#F6F6F6] w-full p-10 flex justify-center items-center ">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6 w-2/3 justify-items-center">
+          {tools.map((tool, index) => (
+            <div
+              key={index}
+              className="p-6 bg-background rounded-lg shadow-md w-52"
+            >
+              <Image
+                src={tool}
+                alt={`Tool ${index + 1}`}
+                width={100}
+                height={100}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
