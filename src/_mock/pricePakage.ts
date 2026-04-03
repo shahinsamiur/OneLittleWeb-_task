540f259b66b53488ff2e3e6265f57d220a65a3c5
shahinsamiur <shahinsamiur@yahoo.com>
2026-04-03 12:28:06 +0600

    feat: price section design complete

---
+++ src/_mock/pricePakage.ts
@@ -0,0 +1,129 @@
+export const pricePakage = [
+  {
+    id: 3,
+    name: "Freebie",
+    price: "$0",
+    priceYearly: "$0",
+    desciption:
+      "Ideal for individuals who need quick access to basic features.",
+    features: [
+      {
+        name: "20,000+ of PNG & SVG graphics",
+        abailable: true,
+      },
+      {
+        name: "Access to 100 million stock images",
+        abailable: true,
+      },
+      {
+        name: "Upload custom icons and fonts",
+        abailable: false,
+      },
+      {
+        name: "Unlimited Sharing",
+        abailable: false,
+      },
+      {
+        name: "Upload graphics & video in up to 4k",
+        abailable: false,
+      },
+      {
+        name: "Unlimited Projects",
+        abailable: false,
+      },
+      {
+        name: "Instant Access to our design system",
+        abailable: false,
+      },
+      {
+        name: "Create teams to collaborate on designs",
+        abailable: false,
+      },
+    ],
+  },
+
+  {
+    id: 2,
+    name: "Professional",
+    price: "$25",
+    priceYearly: "$225",
+    desciption:
+      "Ideal for individuals who, who need advanced features and tools for client work.",
+    features: [
+      {
+        name: "20,000+ of PNG & SVG graphics",
+        abailable: true,
+      },
+      {
+        name: "Access to 100 million stock images",
+        abailable: true,
+      },
+      {
+        name: "Upload custom icons and fonts",
+        abailable: true,
+      },
+      {
+        name: "Unlimited Sharing",
+        abailable: true,
+      },
+      {
+        name: "Upload graphics & video in up to 4k",
+        abailable: true,
+      },
+      {
+        name: "Unlimited Projects",
+        abailable: true,
+      },
+      {
+        name: "Instant Access to our design system",
+        abailable: false,
+      },
+      {
+        name: "Create teams to collaborate on designs",
+        abailable: false,
+      },
+    ],
+  },
+  {
+    id: 1,
+    name: "Enterprise",
+    price: "$100",
+    priceYearly: "$900",
+    desciption:
+      "Ideal for businesses who need personalized services and security for large teams. ",
+    features: [
+      {
+        name: "20,000+ of PNG & SVG graphics",
+        abailable: true,
+      },
+      {
+        name: "Access to 100 million stock images",
+        abailable: true,
+      },
+      {
+        name: "Upload custom icons and fonts",
+        abailable: true,
+      },
+      {
+        name: "Unlimited Sharing",
+        abailable: true,
+      },
+      {
+        name: "Upload graphics & video in up to 4k",
+        abailable: true,
+      },
+      {
+        name: "Unlimited Projects",
+        abailable: true,
+      },
+      {
+        name: "Instant Access to our design system",
+        abailable: true,
+      },
+      {
+        name: "Create teams to collaborate on designs",
+        abailable: true,
+      },
+    ],
+  },
+];
--- src/components/pakageSection.tsx
+++ src/components/pakageSection.tsx
@@ -1,22 +1,80 @@
-import React from "react";
-// import { Switch } from "@/components/ui/switch";
+"use client";
 
-export default function priceSection() {
+import Switch from "@/src/utils/switchButton";
+import Image from "next/image";
+import { Button } from "../utils";
+import { pricePakage } from "../_mock/pricePakage";
+import { FaCheck } from "react-icons/fa6";
+import { RxCross1 } from "react-icons/rx";
+export default function PriceSection() {
   return (
-    <div>
-      <section className="flex flex-col justify-center items-center gap-6">
-        <h1 className="w-full text-center text-3xl">
-          Airnbn Assistent <span className="text-foreground">pricing</span>
-        </h1>
-        <p className=" text-center font-medium">
-          Choose a plan that’s right for you
-        </p>
-        <div className="flex justify-center items-center gap-4">
-          <p>Pay Monthly </p>
-          {/* <Switch /> */}
-          <p>Pay Yearly</p>
-        </div>
-      </section>
-    </div>
+    <section className="flex flex-col justify-center items-center gap-6 w-full py-10">
+      <h1 className="w-full text-center text-3xl font-semibold">
+        Airbnb Assistant <span className="text-foreground">Pricing</span>
+      </h1>
+
+      <p className="text-center font-medium">
+        Choose a plan that’s right for you
+      </p>
+
+      {/* Switch */}
+      <div className="flex justify-center items-center gap-4 relative">
+        <p>Pay Monthly</p>
+        <Switch />
+        <p>Pay Yearly</p>
+
+        <Image
+          src="/save_svg.svg"
+          alt="save"
+          className="w-32 absolute hidden md:block lg:-top-2 lg:-right-32"
+          width={900}
+          height={900}
+        />
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full max-w-6xl mt-8">
+        {pricePakage.map((item) => (
+          <div
+            key={item.id}
+            className={`border border-border rounded-2xl p-6 shadow-md flex flex-col justify-between ${item.id == 2 ? "bg-foreground text-background" : ""}`}
+          >
+            <div className="space-y-4">
+              <h2 className="text-xl font-semibold">{item.name}</h2>
+              <p className="text-sm ">{item.desciption}</p>
+
+              <div className="flex items-end gap-2">
+                <h1 className="text-4xl font-bold">{item.price}</h1>
+                <p className="text-sm ">/ Month</p>
+              </div>
+
+              <Button
+                className={`${item.id == 2 ? "bg-background! text-foreground!" : "bg-background! border-border! border! text-text! hover:bg-foreground! hover:text-background!"} text-background w-full`}
+              >
+                Get Started Now
+              </Button>
+            </div>
+
+            <div className="mt-6 space-y-2">
+              {item.features.map((feature, index) => (
+                <div key={index} className="flex items-center gap-2 text-sm">
+                  {feature.abailable ? (
+                    <span className="size-8 bg-[#FAC4D2] rounded-full flex justify-center items-center">
+                      <FaCheck className="text-foreground" />
+                    </span>
+                  ) : (
+                    <span className="size-8 bg-[#F7F8F9] rounded-full flex justify-center items-center">
+                      <RxCross1 className="text-[#191D23]" />
+                    </span>
+                  )}
+                  <span className={feature.abailable ? "" : "opacity-50"}>
+                    {feature.name}
+                  </span>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 }
